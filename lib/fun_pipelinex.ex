d03057f3aa5051659e094d5769f92d3320cddb19
lizhaochao <lzc@lizhaochao.com>
2021-06-24 22:06:39 +0800

    refactor pipeline macro

--- lib/fun_pipelinex.ex
+++ lib/fun_pipelinex.ex
@@ -8,36 +8,21 @@ defmodule FunPipelinex do
 
   defmacro __using__(_options) do
     quote do
-      Module.register_attribute(__MODULE__, :pipelines, accumulate: true)
       Module.register_attribute(__MODULE__, :filters, accumulate: true)
 
       require Logger
       import unquote(__MODULE__), only: [pipeline: 2, pipe_through: 2]
-      #  @before_compile unquote(__MODULE__)
     end
   end
 
-  #  defmacro __before_compile__(env) do
-  #    Module.get_attribute(env.module, :pipelines) |> Enum.dedup()
-  #    Module.get_attribute(env.module, :filters) |> Enum.dedup()
-  #  end
-
   defmacro pipeline(name, do: block) do
-    pipeline_expr =
+    block
+    |> Helper.parse_filters()
+    |> Enum.map(fn filter ->
       quote do
-        @pipelines unquote(name)
+        @filters {unquote(name), unquote(filter)}
       end
-
-    filters_expr =
-      block
-      |> Helper.parse_filters()
-      |> Enum.map(fn filter ->
-        quote do
-          @filters unquote(filter)
-        end
-      end)
-
-    [pipeline_expr] ++ filters_expr
+    end)
   end
 
   defmacro pipe_through(pipelines, do: block) do
